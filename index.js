da14d918369b080985ceb1f2b6e0a76ebd8da8e1
Behzad <b.soleimani@rismun.com>
2022-03-02 14:04:29 +0330

    adding test and gitignore to project

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ const readline = require('readline').createInterface({
 const calculateFee = (arrayInput) => {
   const stringToArray = JSON.parse(arrayInput)
   let store = [];
+  let results = []
   stringToArray.forEach(element => {
     if (element.type === "cash_in") {
       const initialFee = element.operation.amount * 0.03 / 100;
       const result = initialFee > 5 ? 5 : initialFee;
-      console.log(Math.ceil(result).toFixed(2))
+      results.push(Math.ceil(result).toFixed(2))
     } else if (element.user_type === "natural") {
       const findUser = store.findIndex(i => i.user_id === element.user_id)
       if (findUser === -1) {
         store.push({ user_id: element.user_id, amount: element.operation.amount, date: element.date });
         const initialFee = element.operation.amount <= 1000 ? 0 : (element.operation.amount - 1000) * 0.3 / 100;
-        console.log(Math.ceil(initialFee).toFixed(2))
+        results.push(Math.ceil(initialFee).toFixed(2))
       } else {
         const thisWeek = (new Date(element.date) - new Date(store[findUser].date)) / 86400000;
         if (thisWeek < 7) {
@@ -36,10 +37,10 @@ const calculateFee = (arrayInput) => {
         }
         if (store[findUser].freeFee) {
           const initialFee = element.operation.amount * 0.3 / 100;
-          console.log(Math.ceil(initialFee).toFixed(2))
+          results.push(Math.ceil(initialFee).toFixed(2))
         } else {
           const initialFee = element.operation.amount <= 1000 ? 0 : (element.operation.amount - 1000) * 0.3 / 100;
-          console.log(Math.ceil(initialFee).toFixed(2))
+          results.push(Math.ceil(initialFee).toFixed(2))
         }
 
       }
@@ -47,14 +48,20 @@ const calculateFee = (arrayInput) => {
     } else {
       const initialFee = element.operation.amount * 0.3 / 100;
       const result = initialFee < 0.5 ? 0.5 : initialFee;
-      console.log(result.toFixed(2))
+      results.push(result.toFixed(2))
     }
   });
-
-}
+  console.log(results.join("\n"));
+  return results.join("\n");
+};
 
 readline.question(`Please Enter the name of your file (or if you want, you can put your valid array json in text.json)`, fileName => {
   const content = fs.readFileSync(`${fileName || "text"}.json`, 'utf8');
   calculateFee(content)
   readline.close()
-})
+});
+
+
+module.exports = {
+  calculateFee
+}
---
+++ test.js
@@ -0,0 +1,14 @@
+const { calculateFee} = require('./index');
+const fs = require("fs");
+
+
+test('gettin an array and return expectable result', async () => {
+    const content = fs.readFileSync(`text.json`, 'utf8');
+    expect(calculateFee(content)).toStrictEqual([
+        '1.00',  '0.90',
+        '87.00', '3.00',
+        '1.00',  '1.00',
+        '5.00',  '0.00',
+        '0.00'
+      ].join("\n"))
+})
